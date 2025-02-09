51e7a4b79ff939f2257e430a87f45b1f2d7d5c59
Josemaria Bolanos <josemabol@gmail.com>
2025-02-08 18:48:19 -0600

    Fix bug trying to add settings for non-admin users

--- settings.php
+++ settings.php
@@ -9,30 +9,32 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-// Create a tool settings page for new registrations pending authorization.
-$settings = new admin_settingpage(
-    'enrolsettingslti_pendingregistrations',
-    get_string('pendingregistrations', 'local_lti_registration'),
-    'moodle/site:config',
-    $this->is_enabled() === false
-);
-
-$settings->add(
-    new admin_setting_configcheckbox(
-        'local_lti_registration/autoapprove',
-        get_string('autoapprove', 'local_lti_registration'),
-        get_string('autoapprove_desc', 'local_lti_registration'),
-        1
-    )
-);
-
-$settings->add(
-    new admin_setting_heading(
-        'enrol_lti_tool_pendingregistrations_heading',
+if ($ADMIN->locate('enrollitfolder')) {
+    // Create a tool settings page for new registrations pending authorization.
+    $settings = new admin_settingpage(
+        'enrolsettingslti_pendingregistrations',
         get_string('pendingregistrations', 'local_lti_registration'),
-        ''
-    )
-);
-$settings->add(new \local_lti_registration\admin_setting_pendingregistrations());
+        'moodle/site:config',
+        $this->is_enabled() === false
+    );
 
-$ADMIN->add('enrolltifolder', $settings);
+    $settings->add(
+        new admin_setting_configcheckbox(
+            'local_lti_registration/autoapprove',
+            get_string('autoapprove', 'local_lti_registration'),
+            get_string('autoapprove_desc', 'local_lti_registration'),
+            1
+        )
+    );
+
+    $settings->add(
+        new admin_setting_heading(
+            'enrol_lti_tool_pendingregistrations_heading',
+            get_string('pendingregistrations', 'local_lti_registration'),
+            ''
+        )
+    );
+    $settings->add(new \local_lti_registration\admin_setting_pendingregistrations());
+
+    $ADMIN->add('enrolltifolder', $settings);
+}
